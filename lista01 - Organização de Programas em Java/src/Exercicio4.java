5c2ae3ab3eb1d13aa2c49cc6f4259177c3d4b64a
Yasmin Macedo <yasminmacedo_11@outlook.com>
2025-04-15 00:51:03 -0300

    feat: adicionando resolucao do ultimo exercicio da lista01

--- lista01 - Organização de Programas em Java/src/Exercicio4.java
+++ lista01 - Organização de Programas em Java/src/Exercicio4.java
@@ -2,15 +2,45 @@ import java.util.Scanner;
 
 public class Exercicio4 {
     public static void main(String[] args) {
-        int x, real1, cent50, cent25, cent10, cent5, cent1;
+        int x, moeda1Real = 0, moeda50Cent = 0, moeda25Cent = 0, moeda10Cent = 0, moeda5Cent = 0, moeda1Cent = 0;
         Scanner teclado = new Scanner(System.in);
 
         System.out.println("Digite o valor em centavos");
         x = teclado.nextInt();
+        teclado.close();
 
-        if (x % 100 != 0) {
-            
+        while (x != 0) {
+            if (x < 100) {
+                if (x < 50) {
+                    if (x < 25) {
+                        if (x < 10) {
+                            if (x < 5) {
+                                x = x - 1;
+                                moeda1Cent += 1;
+                            } else {
+                                x = x - 5;
+                                moeda5Cent += 1;
+                            }
+                        } else {
+                            x = x - 10;
+                            moeda10Cent += 1;
+                        }
+                    } else {
+                        x = x - 25;
+                        moeda25Cent += 1;
+                    }
+                } else {
+                    x = x - 50;
+                    moeda50Cent += 1;
+                }
+            } else {
+                x = x - 100;
+                moeda1Real += 1;
+            }
         }
         
+        System.out.println(moeda1Real + " moeda(s) de 1 real, " + moeda50Cent + " moeda(s) de 50 centavos, " 
+        + moeda25Cent + " moeda(s) de 25 centavos, " + moeda10Cent + " moeda(s) de 10 centavos, " 
+        + moeda5Cent + " moeda(s) de 5 centavos, " + moeda1Cent + " moeda(s) de 1 centavo.");
     }
 }
